7722133ae82dfe62e797a04fbc7a62c9138417ea
bradarshome <aduhay@mail>
2026-03-27 11:06:48 +0000

    Add dynamic partitions config to device.mk

--- device.mk
+++ device.mk
@@ -1,8 +1,13 @@
 #
 # Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2026 SkyHawk Recovery Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from mt6789-common
 $(call inherit-product, device/transsion/mt6789-common/common.mk)
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETAIN_PERSISTENT_PARTITIONS := true
